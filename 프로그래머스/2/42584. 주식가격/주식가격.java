1bcd21ab74d00572727024102d5584f1be2e444a
kms7522 <75304079+kms7522@users.noreply.github.com>
2026-06-29 22:02:15 +0900

    [level 2] Title: 주식가격, Time: 21.38 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,5 +1,10 @@
 import java.util.*;
-
+/*
+1. for문 돌려서 인덱스를 스택에 푸시
+2, 다음 가격과 비교해 더 크면 패스
+3. 작으면 answer 배열의 그 인덱스 자리에 현재 i - 인덱스 추가
+4. for문 끝나면 스택에 남은거 length-1 - 인덱스 answer 배열에 추가
+*/
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
