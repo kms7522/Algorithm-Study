70cb629fb7225e9d5a8953ad267bef13e60f1b8a
kms7522 <75304079+kms7522@users.noreply.github.com>
2026-07-01 09:36:25 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.38 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,3 +1,24 @@
+/*
+1. 한글자씩 나눈 String 배열로 저장
+2. 공백이면 sb 넣고 true 바꾸고 패스
+3. 첫번째 글자면 대문자로 넣음
+4. 아니면 소문자로 넣음
+
+public String solution(String s) {
+    String answer = "";
+    String[] sp = s.toLowerCase().split("");
+    boolean flag = true;
+
+    for(String ss : sp) {
+        answer += flag ? ss.toUpperCase() : ss;
+        flag = ss.equals(" ") ? true : false;
+    }
+
+    return answer;
+}
+  처음 넣을때 소문자로 만들고 삼항연산자도 이용하면 더 깔끔하게 쓸수 있음
+*/
+
 class Solution {
     public String solution(String s) {
         String[] arr = s.split("");
