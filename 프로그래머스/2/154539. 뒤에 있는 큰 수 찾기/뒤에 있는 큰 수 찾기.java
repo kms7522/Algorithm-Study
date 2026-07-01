8646668dae5bde55cd99a5e9a6f12a6eb49fd2f5
kms7522 <75304079+kms7522@users.noreply.github.com>
2026-07-01 12:05:05 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 170.00 ms, Memory: 208 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,5 +1,10 @@
 import java.util.*;
-
+/*
+스택 문제
+1. 스택이 비거나 탑이 number보다 커질때까지 pop함
+2. 스택이 비어있으면 -1, 아니면 스택의 탑 넣음
+3. 스택에 number 푸시함
+*/
 class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
