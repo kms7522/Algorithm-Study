dfa90b2c4fe58204b006f0a1d62047ba394f9225
kms7522 <75304079+kms7522@users.noreply.github.com>
2026-07-01 16:40:52 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 5.88 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,5 +1,11 @@
 import java.util.*;
 
+/*
+1. 문자열 " "로 스플릿
+2. parseInt로 정수로 바꾼뒤 정수 배열에 추가
+3. 정수 배열 sort후 첫번째 인덱스와 마지막 인덱스 리턴
+*/
+
 class Solution {
     public String solution(String s) {
         String answer = "";
