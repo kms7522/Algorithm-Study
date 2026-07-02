c7697cc572502c74139cc116cdf86be3e615a95e
kms7522 <75304079+kms7522@users.noreply.github.com>
2026-07-02 15:43:18 +0900

    [level 1] Title: 기사단원의 무기, Time: 7828.83 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,3 +1,14 @@
+/*
+각각 약수 구해서 limit와 비교해서 더해줌
+    int[] count = new int[number + 1];    
+    for (int i = 1; i <= number; i++) {
+        for (int j = 1; j <= number / i; j++) {
+            count[i * j]++;
+        }
+    }
+    약수 이렇게 구하면 nlogn 시간복잡도 
+    각각 number까지 i의 배수에 다 1 더해줘서 약수 구해줌
+*/
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
