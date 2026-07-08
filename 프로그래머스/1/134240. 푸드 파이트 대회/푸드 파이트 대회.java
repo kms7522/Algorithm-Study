9c6c62e45ca7be2256ffc9453a28579134878c61
kms7522 <75304079+kms7522@users.noreply.github.com>
2026-07-08 12:45:17 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.12 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,3 +1,16 @@
+/*
+String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
+            }
+        }
+
+        return answer;
+StringBuilder 사용해서 위처럼하는게 더 좋으듯
+*/
+
 class Solution {
     public String solution(int[] food) {
         StringBuilder sb = new StringBuilder();
