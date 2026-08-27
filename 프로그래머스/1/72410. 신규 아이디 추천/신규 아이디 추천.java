2ad95493ce459344bdb8f17ec633149593aabb90
kms7522 <75304079+kms7522@users.noreply.github.com>
2026-08-27 09:35:51 +0900

    [level 1] Title: 신규 아이디 추천, Time: 6.41 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,50 @@
+class Solution {
+    public String solution(String new_id) {
+        StringBuilder sb = new StringBuilder();
+        
+        String answer = new_id.toLowerCase();
+        
+        for (int i = 0; i < answer.length(); i++) {
+            char c = answer.charAt(i);
+            if ((c >= 'a' && c <= 'z') ||
+                (c >= '0' && c <= '9') ||
+                c == '-' || c == '_' || c == '.') {
+                sb.append(c);
+            }
+        }
+        
+        answer = sb.toString();
+        
+        while(answer.contains("..")){
+            answer = answer.replace("..", ".");
+        }
+        
+        if(!(answer.isEmpty()) && answer.charAt(0) == '.') {
+            answer = answer.substring(1);
+        }
+        
+        if(!(answer.isEmpty()) && 
+            answer.charAt(answer.length() - 1) == '.') {
+            answer = answer.substring(0, answer.length() - 1);
+        }
+        
+        if(answer.isEmpty()) {
+            answer += 'a';
+        }
+        
+        if(answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+        }
+        
+        if(!(answer.isEmpty()) && 
+            answer.charAt(answer.length() - 1) == '.') {
+            answer = answer.substring(0, answer.length() - 1);
+        }
+        
+        while(answer.length() < 3) {
+            answer += answer.charAt(answer.length() - 1);
+        }
+        
+        return answer;
+    }
+}
