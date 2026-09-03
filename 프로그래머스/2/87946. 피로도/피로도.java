3ace8b35667779352ce6ace7b07588d5d82e1aaf
kms7522 <75304079+kms7522@users.noreply.github.com>
2026-09-03 09:36:41 +0900

    [level 2] Title: 피로도, Time: 2.58 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,25 @@
+class Solution {
+    boolean[] visited;
+    int answer;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0);
+        return answer;
+    }
+    
+    void dfs(int k, int[][] dungeons, int cnt) {
+        answer = Math.max(answer, cnt);
+        
+        for (int i = 0; i < dungeons.length; i++) {
+
+            if (!visited[i] && k >= dungeons[i][0]) {
+
+                visited[i] = true;
+
+                dfs(k - dungeons[i][1], dungeons, cnt + 1);
+
+                visited[i] = false;
+            }
+        }
+    }
+}
