3f43aad32c3d3b804f08f71726d90377256d1008
kms7522 <75304079+kms7522@users.noreply.github.com>
2026-09-07 10:31:10 +0900

    [level 2] Title: 오픈채팅방, Time: 83.91 ms, Memory: 168 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] record) {
+        HashMap<String, String> hm = new HashMap<>();
+        for(int i=0; i<record.length; i++) {
+            String[] str = record[i].split(" ");
+            if(str[0].equals("Change")) {
+                hm.replace(str[1], str[2]);
+            }
+            else if(str[0].equals("Enter")){
+                hm.put(str[1], str[2]);
+            }
+        }
+        ArrayList<String> arr = new ArrayList<>();
+        for(int i=0; i<record.length; i++) {
+            String[] str = record[i].split(" ");
+            String nickname = hm.get(str[1]);
+            if(str[0].equals("Enter")) {
+                arr.add(nickname + "님이 들어왔습니다.");
+            }
+            else if(str[0].equals("Leave")) {
+                arr.add(nickname + "님이 나갔습니다.");
+            }
+        }
+        String[] answer = new String[arr.size()];
+        for(int i=0; i<arr.size(); i++) {
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
